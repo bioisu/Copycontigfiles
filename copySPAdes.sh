8afc86a0fb116c8d6298f5256491e6a1d9789e80
bioisu <lizhen@isu.edu>
2018-01-29 09:54:57 -0800

    Update copySPAdes.sh

--- copySPAdes.sh
+++ copySPAdes.sh
@@ -2,7 +2,7 @@
 ##############################
 ### Copywright Zhen Li 2017### 
 ##############################
-# This script is used after SPAdes de novo assemble. 
+# This script is used after SPAdes de novo assembly of bacterial genomes. 
 # It copies the contigs.fasta file in each sample folder into a fasta file named after the sample folder name. 
 # It then moves the new fasta files from each folder into a new folder name "contigfiles". 
 
